e6b480011cb7e3e569814e148e96b493941ac64d
xiangying <1984997880@qq.com>
2023-03-13 12:30:35 +0800

    Revert "[improve][broker] Require authRole is proxyRole to set originalPrincipal (#19455)"
    This reverts commit 6a599af216f4e7fe09e31732d128eccc9c2d7ec4.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -4104,7 +4104,7 @@ public class PersistentTopicsBase extends AdminResource {
                 });
     }
 
-    public CompletableFuture<PartitionedTopicMetadata> getPartitionedTopicMetadata(
+    public static CompletableFuture<PartitionedTopicMetadata> getPartitionedTopicMetadata(
             PulsarService pulsar, String clientAppId, String originalPrincipal,
             AuthenticationDataSource authenticationData, TopicName topicName) {
         CompletableFuture<PartitionedTopicMetadata> metadataFuture = new CompletableFuture<>();
